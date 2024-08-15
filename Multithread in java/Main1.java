aa264b06fc9cc3ec2275eb0e9377057dc761741f
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-16 00:32:19 +0600

    Update Main1.java

--- Multithread in java/Main1.java
+++ Multithread in java/Main1.java
@@ -1,1 +1,16 @@
+package multiThread;
+
+public class Main1 {
+    public static void main(String[] args) {
+        Thread t=new Thread(new Runnable(){
+            public void run(){
+                System.out.println("Dipta");
+            }
+    });
+        
+        t.start();
+}
+}
+
+
 
