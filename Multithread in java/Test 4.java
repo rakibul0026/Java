ddb884c925da55fc390af33c6709d73270cd11d2
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-16 00:19:44 +0600

    Update Test 4.java

--- Multithread in java/Test 4.java
+++ Multithread in java/Test 4.java
@@ -1,1 +1,26 @@
+package multiThread;
+public class Test4 {
+     public static void main(String[] args) {
+       myThread obj=new  myThread();
+      
+      Thread t1=new Thread(obj,"my thread 1");//thread name
+      t1.start();//create thread
+       
+ System.out.println("inside the main method: "+Thread.currentThread().getName());//current Thread
+     
+    } 
+}
+class  myThread extends A implements Runnable{
+    public void run(){
+        System.out.println("Inside the run method :"+Thread.currentThread().getName());
+        for(int i=0;i<3;i++){
+            System.out.println("1");
+            try {
+                Thread.sleep(500);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
 
