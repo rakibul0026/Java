e99542b88a6f644b2534a26baadc588a5a856974
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-16 00:30:24 +0600

    Update Test 6 .java

--- Multithread in java/Test 6 .java
+++ Multithread in java/Test 6 .java
@@ -1,1 +1,24 @@
 
+package multiThread;
+public class Test6 {
+    public static void main(String[] args) {
+        System.out.println("Main Thread started");
+        Dipta t1=new Dipta();//thread class ar object
+        //System.out.println(t1.isAlive());
+        t1.start(); //create thread class
+        System.out.println(t1.isAlive());
+ System.out.println("Main Thread ended and x="+t1.x);
+        
+    }
+}
+class Dipta extends Thread{
+    int x;
+    public void run(){
+        for(int i=0;i<10000;i++){
+            x++;
+            
+        }
+      System.out.println("Main Thread ended and x="+x);
+    }
+    
+}
