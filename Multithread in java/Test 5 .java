8dc36579fb7451385a5e9f451579c33fc4b73092
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-16 00:20:28 +0600

    Update Test 5 .java

--- Multithread in java/Test 5 .java
+++ Multithread in java/Test 5 .java
@@ -1,1 +1,23 @@
 
+package multiThread;
+public class Test5 {
+    public static void main(String[] args) {
+        Dipta t1=new Dipta();
+        t1.start();
+        try {
+            t1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(t1.x);
+        }
+}
+class Dipta extends Thread{
+    int x;
+    public void run(){
+        for(int i=0;i<10000;i++){
+            x++;
+        }
+    }
+    
+}
