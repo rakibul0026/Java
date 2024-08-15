e96d98b16cadeb2ff36096c9fb42a1b59d7ee4b7
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-16 00:18:03 +0600

    Update TEST 1.java

--- Multithread in java/TEST 1.java
+++ Multithread in java/TEST 1.java
@@ -1,1 +1,39 @@
-
+package multiThread;
+public class Test {
+    public static void main(String[] args)throws InterruptedException {
+      A obj=new A();//create a Thread step-1
+      obj.start(); //t1
+    
+for(int i=0;i<10;i++)
+    {
+            System.out.println(2);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) 
+        {
+            System.out.println(e);
+        }
+       }
+     }
+        
+ }
+    
+    
+class A extends Thread{
+    @Override
+    public void run(){
+        for(int i=0;i<10;i++)
+    {
+            System.out.println(1);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) 
+        {
+            e.printStackTrace();
+        }
+    }
+    }
+    public void print(){
+    
+      }
+    }
