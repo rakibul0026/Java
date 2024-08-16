3baeee2692b819472b7662253ec64d0455f8ab8b
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-17 02:05:01 +0600

    Create student.java

---
+++ Encapsulation/p1/student.java
@@ -0,0 +1,20 @@
+package Encapsulation;
+public class student {
+   private  String name;
+    private int roll;
+    
+    public void set(String name){
+        this.name=name;
+    }
+    public String get(){
+        return name;
+    }
+    public void setroll(int roll){
+        this.roll=roll;
+        
+    }
+    public int getroll(){
+        return roll;
+    }
+    
+}
