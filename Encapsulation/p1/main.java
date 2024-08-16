31ebc73c9c55aab0ba8844c845ce618e48733db1
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-17 01:54:08 +0600

    Update main.java

--- Encapsulation/p1/main.java
+++ Encapsulation/p1/main.java
@@ -1,1 +1,14 @@
+package Encapsulation;
+
+public class main {
+    public static void main(String[] args) {
+         student ob=new  student();
+         ob.set("ratul");
+         ob.setroll(23);
+         
+         System.out.println("name is :"+ob.get());
+         System.out.println("roll is :"+ob.getroll());
+    }
+ 
+}
 
