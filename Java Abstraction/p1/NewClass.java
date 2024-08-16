190892c9d5339c05bd0516ff453f58034e97ef70
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-17 02:10:55 +0600

    Update NewClass.java

--- Java Abstraction/p1/NewClass.java
+++ Java Abstraction/p1/NewClass.java
@@ -1,1 +1,15 @@
 
+package Demo.abstraction;
+
+public   abstract class A {
+     abstract void print();
+     void display(){
+         System.out.println(" CSE");
+     }
+    
+}
+class B extends A{
+void print()  {
+    System.out.println("cstu");
+}  
+}
