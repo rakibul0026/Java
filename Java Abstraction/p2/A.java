771ab173c774842bffa33985a0ad33b0a00f5897
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-17 02:14:56 +0600

    Update A.java

--- Java Abstraction/p2/A.java
+++ Java Abstraction/p2/A.java
@@ -1,1 +1,14 @@
 
+package Demo.newpackage1;
+
+public class A {
+  void print (){
+      System.out.println("wellcome to cstu");
+  }
+}
+class B extends A {
+    @Override
+    void print (){
+      System.out.println("wellcome to CSE");
+  }
+}
