0c859bce016354106915cf29f4c418f47a81819b
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-17 02:15:25 +0600

    Update main.java

--- Java Abstraction/p2/main.java
+++ Java Abstraction/p2/main.java
@@ -1,1 +1,14 @@
 
+package Demo.newpackage1;
+
+public class main {
+    public static void main(String[] args) {
+        B ob =new B();
+        ob.print();
+        A ob2 =new A();
+        ob2.print();
+        
+    }
+    
+}
+
