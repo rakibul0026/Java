b25368620edf605c73d70f9c90f2e925a6616089
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-17 01:53:41 +0600

    Update main.java

--- Encapsulation/p1/main.java
+++ Encapsulation/p1/main.java
@@ -1,14 +1,1 @@
-package constructor;
-public class AB {
-    int a;
-    int b;
- AB(int a,int b){
-        
-        this.a=a;
-        this.b=b;
-        System.out.println("a :"+a);
-        System.out.println("b :"+b);
-    }
-    
-}
 
