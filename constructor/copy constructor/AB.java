21af5a6b9620427bb68a5e37223fc7d00d87453e
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-18 01:28:50 +0600

    Update AB.java

--- constructor/copy constructor/AB.java
+++ constructor/copy constructor/AB.java
@@ -1,1 +1,15 @@
+package copyconstructor;
+public class AB {
+    String name;
+    int id;
+    AB(String name,int id){
+        this.name=name;
+        this.id=id;
+        }
+    AB(AB ob2){
+       this.name=ob2.name;
+        this.id=ob2.id; 
+    }
+    
+}
 
