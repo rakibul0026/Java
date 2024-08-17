53405508af54c68051098e414c1437aea0739774
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-18 01:29:17 +0600

    Update main.java

--- constructor/copy constructor/main.java
+++ constructor/copy constructor/main.java
@@ -1,1 +1,16 @@
+package copyconstructor;
+public class main {
+    public static void main(String[] args) {
+        AB ob=new AB("ratul",1026);
+        System.out.println("name is "+ob.name);
+        System.out.println("id is :"+ob.id);
+        
+        
+        
+        AB ob2=new AB(ob);
+        System.out.println("After copy constructor :"+ob2.name);
+        System.out.println("After copy constructor :"+ob2.id);
+        
+    }
+}
 
