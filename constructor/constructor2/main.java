12c0e47caf2c1f6eb87991b60ea64261195dce53
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-18 01:35:54 +0600

    Update main.java

--- constructor/constructor2/main.java
+++ constructor/constructor2/main.java
@@ -1,1 +1,19 @@
+package constructor.newpackage;
+public class main {
+    public static void main(String[] args) {
+         Example ob=new Example("ratul",12);
+         System.out.println("name :"+ob.name);
+         System.out.println("id :"+ob.id);
+         
+         
+          Example ob2=new  Example(ob);
+          System.out.println("");
+          System.out.println("After use copy constructor : "+ob2.name);
+          System.out.println("after use copy constructor is :"+ob2.id);
+         
+         
+         
+    }
+    
+}
 
