9b5d8e79fd1f36207f47a18641d3a2fb25b6c5df
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-18 01:35:26 +0600

    Update Example.java

--- constructor/constructor2/Example.java
+++ constructor/constructor2/Example.java
@@ -1,1 +1,15 @@
+package constructor.newpackage;
+public class Example {
+    String name ;
+    int id;
+    Example(String name ,int id){
+        this.name=name;
+        this.id=id;
+        
+    }
+    Example(Example ob2){
+        this.name=ob2.name;
+        this.id=ob2.id;
+    }
+}
 
