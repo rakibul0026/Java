2831e096be7fed9147c9825e2032e5281d223373
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-19 01:45:47 +0600

    Update fileWrite.java

--- Java File /File3/fileWrite.java
+++ Java File /File3/fileWrite.java
@@ -1,1 +1,17 @@
+package file23;
+
+import java.io.FileWriter;
+
+public class fileWriter {
+    public static void main(String[] args) {
+        try{
+            FileWriter f=new FileWriter("D:\\oop question class test//cstu.txt");
+            f.write("ratul khan");
+            f.close();
+        }catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+}
 
