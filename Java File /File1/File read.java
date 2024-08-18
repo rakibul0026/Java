2fafe244cc1c407dca3c10478b3ca6396bf7a86b
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-19 01:10:57 +0600

    Update File read.java

--- Java File /File1/File read.java
+++ Java File /File1/File read.java
@@ -1,1 +1,25 @@
 
+package file;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class FileRead {
+    public static void main(String[] args) {
+        try{
+          File file=new  File("D:\\Math class note//.txt");
+            Scanner reader=new  Scanner(file);
+            while(reader.hasNext()){
+                String Id=reader.next();
+                String name=reader.next();
+                System.out.println(Id);
+                System.out.println(name);
+                
+            reader.close();
+           
+        }
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
