7b0caf02a83c8a9b5583dfe16c48b06cb8c9cd50
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-19 01:31:12 +0600

    Update FileNotFountException.java

--- Java File /Flle2/FileNotFountException.java
+++ Java File /Flle2/FileNotFountException.java
@@ -1,1 +1,22 @@
+package file.newpackage;
+
+public class FileNotFoundException extends Exception {
+    
+    public FileNotFoundException() {
+        super();
+    }
+    
+    public FileNotFoundException(String message) {
+        super(message);
+    }
+    
+    public FileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    
+    public FileNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
+
 
