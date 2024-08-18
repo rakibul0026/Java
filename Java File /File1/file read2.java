d14ff6abefad2d1c6e7a26515ce1ebd8fca7ad92
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-19 01:11:24 +0600

    Update file read2.java

--- Java File /File1/file read2.java
+++ Java File /File1/file read2.java
@@ -1,1 +1,22 @@
+package file;
+import java.io.File;
+import java.util.Scanner;
+public class fileread32 {
+    public static void main(String[] args) {
+        try{
+            File file=new File("D:\\prince sir class note Discrete mathmatics//cse.txt");
+            Scanner reader= new Scanner(file);
+    while(reader.hasNext()){
+       String name= reader.next();
+       String id=reader.next();
+        System.out.println(name);
+        System.out.println(id);
+        
+        
+    }
+} catch(Exception e){
+            System.out.println(e);
+}
+    }
+}
 
