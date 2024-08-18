10628e396ef4da4be23226e0b4f4524d49890899
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-19 01:44:54 +0600

    Update File Demo 3.java

--- Java File /File3/File Demo 3.java
+++ Java File /File3/File Demo 3.java
@@ -1,1 +1,21 @@
 
+package file23;
+
+import java.io.File;
+
+public class fileDemo3 {
+    public static void main(String[] args) {
+        File dir=new File("//person");
+        dir.mkdir();
+        
+        File file=new File ("D:\\person//cse.txt");
+        try{
+            file.createNewFile();
+            System.out.println("file is create");
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        
+    }
+}
+
