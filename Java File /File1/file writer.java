197bc2392527b425b7029f32611a485aecc0e21a
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-19 01:13:34 +0600

    Update file writer.java

--- Java File /File1/file writer.java
+++ Java File /File1/file writer.java
@@ -1,1 +1,19 @@
 
+package file;
+
+import java.io.FileWriter;
+
+public class fileWriter {
+    public static void main(String[] args) {
+       try{
+        FileWriter f= new FileWriter("D:\\person\\person//.txt"); 
+        f.write("r");
+        f.close();
+       }catch(Exception e){
+           System.out.println(e);
+       }
+      
+    }
+    
+}
+
