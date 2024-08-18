b9610d64918f80d5f546fc338060fb8c017cd7f9
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-19 01:44:29 +0600

    Update File Demo 2 .java

--- Java File /File3/File Demo 2 .java
+++ Java File /File3/File Demo 2 .java
@@ -1,1 +1,20 @@
+package file23;
+
+import java.util.Formatter;
+
+public class FileDemo26 {
+    public static void main(String[] args) {
+        try{
+           Formatter formatter =new Formatter("D:\\prince sir class note Discrete mathmatics//Cse.txt");  
+         formatter.format("%s %s\r\n","24","saiyam");
+         formatter.format("%s %s\r\n","25","iftiqur");
+         formatter.format("%s %s\r\n","26","ratul");
+         formatter.close();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+       
+    }
+    
+}
 
