f98eec15c00339dd4b0147a82c28074aacc2b7ce
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-19 01:44:01 +0600

    Update File Demo .java

--- Java File /File3/File Demo .java
+++ Java File /File3/File Demo .java
@@ -1,1 +1,18 @@
 
+package file23;
+
+import java.io.File;
+
+public class fileDemo {
+    public static void main(String[] args) {
+        File dir =new File("D:\\person//name");
+        dir.mkdir();
+        String dirlocation=dir.getAbsolutePath();
+        System.out.println("Dirlocation is : "+dirlocation);
+        System.out.println(dir.getName());
+        dir.delete();
+        
+    }
+    
+}
+
