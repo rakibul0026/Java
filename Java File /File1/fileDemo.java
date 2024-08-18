d5fcad55a24824c553be09aa25fd9be2d692cda3
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-19 01:12:52 +0600

    Update fileDemo.java

--- Java File /File1/fileDemo.java
+++ Java File /File1/fileDemo.java
@@ -1,1 +1,32 @@
+package file;
+import java.io.File;
+import java.io.IOException;
 
+public class fileDemo1 {
+    public static void main(String[] args) {
+        File dir=new File("person");
+        dir.mkdir();
+        String path=dir.getAbsolutePath();
+        File file1=new File(path+"student .txt");
+      File file2=new File(path+"teacher .txt");
+      try{
+          file1.createNewFile();
+          file2.createNewFile();
+          System.out.println("file are created");
+      }catch(IOException e){
+          System.out.println(e);
+          
+     
+    }
+     file2.delete();
+      file1.delete();
+      if (file1.exists()){
+          System.out.println("File is exist");
+      }
+          else
+          System.out.println("file doesnot Exist");
+      
+       file1.delete();
+       file2.delete();
+  }
+}
