11bcba02bace3f8e7baacc111546e1664275fb18
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-19 01:12:20 +0600

    Update filetypeme.java

--- Java File /File1/filetypeme.java
+++ Java File /File1/filetypeme.java
@@ -1,1 +1,24 @@
+package file;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Filetryme {
+    public static void main(String[] args) {
+        try{
+           File file=new File ("D:\\prince sir class note Discrete mathmatics//cse.txt");
+           Scanner reader=new Scanner(file);
+           while(reader.hasNext()){
+               String name=reader.next();
+               String id=reader.next();
+               System.out.println(id);
+               System.out.println(name);
+           }
+        }catch(FileNotFoundException e){
+            System.out.println(e); 
+        }
+    }
+  
+}
 
