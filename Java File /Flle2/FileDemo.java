4ebd4d69d5407fd25b53668c4b3bd8f11ae54167
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-19 01:31:43 +0600

    Update FileDemo.java

--- Java File /Flle2/FileDemo.java
+++ Java File /Flle2/FileDemo.java
@@ -1,1 +1,29 @@
+package file.newpackage;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.util.Formatter;
+
+public class FileDemo2 {
+    public static void main(String[] args) {
+        String id, name;
+        try {
+            Formatter formatter = new Formatter("D:\\CT DISCRETE\\person//teacher.txt");
+            Scanner input = new Scanner(System.in);
+            
+            System.out.println("Enter the number of records:");
+            int num = input.nextInt();
+            
+            for (int i = 0; i < num; i++) {
+                System.out.println("Enter the id and name:");
+                id = input.next();
+                name = input.next();
+                formatter.format("%s %s\r\n", id, name);
+            }
+
+            formatter.close();
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+    }
+}
 
