4d53c0b5d69e59d0776463267edb18cd8c2994b5
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-19 01:45:20 +0600

    Update File read.java

--- Java File /File3/File read.java
+++ Java File /File3/File read.java
@@ -1,1 +1,26 @@
+package file23;
+
+import java.io.File;
+
+import java.util.Scanner;
+
+public class FileRead {
+    public static void main(String[] args) {
+        
+        try{
+          File file=new File("D:\\prince sir class note Discrete mathmatics//cse.txt"); 
+          Scanner scanner =new Scanner(file);
+          while(scanner.hasNext()){
+             String id=scanner.next();
+              String name=scanner.next();
+              System.out.println("the id is:"+id);
+              System.out.println("the name is:"+name);
+          }
+          scanner .close();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        
+    }
+}
 
