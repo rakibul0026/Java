c9d2f8b9f5fc33719fb95dde079faddd354399a1
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-19 01:15:58 +0600

    Update file writer2.java

--- Java File /File1/file writer2.java
+++ Java File /File1/file writer2.java
@@ -1,1 +1,19 @@
 
+
+package file;
+
+import java.io.FileWriter;
+
+public class fileWriter23 {
+    public static void main(String[] args) {
+        try{
+          FileWriter f=new FileWriter("D:\\Math class note//.txt"); 
+          f.write("Wellcome to Bangladesh\n");
+          f.write("Wellcome to CSTU");
+          f.close();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
+
