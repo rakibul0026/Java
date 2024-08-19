16bce794211afa724eeaee0ea21340861f0142be
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-20 01:27:40 +0600

    Update A.java

--- inheritance/p1/A.java
+++ inheritance/p1/A.java
@@ -1,1 +1,20 @@
 
+package inheritance23;
+
+public class A {
+    void print(){
+        System.out.println("cstu");
+    }
+    
+}
+class B extends A{
+    void display(){
+        System.out.println("CSE");
+    }
+}
+class C extends B{
+    void show(){
+        System.out.println("ict");
+    }
+}
+
