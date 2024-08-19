3a44b9a167a76d884e85e49780fabbb7794c7274
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-20 01:30:31 +0600

    Update A.java

--- inheritance/p2/A.java
+++ inheritance/p2/A.java
@@ -1,1 +1,23 @@
 
+package inheritance;
+
+interface A {
+   abstract void display();
+}
+
+interface B {
+   abstract void print();
+    
+}
+
+
+class M implements A,B{
+    @Override
+   public  void display(){
+        System.out.println("CSTU");   
+    }
+    @Override
+    public void print(){
+        System.out.println("CSE");
+    }
+}
