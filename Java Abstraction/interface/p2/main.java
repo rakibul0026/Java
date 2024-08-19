9062f3e498f071672e0f815d74ead53851e3f991
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-20 01:46:30 +0600

    Create main.java

---
+++ Java Abstraction/interface/p2/main.java
@@ -0,0 +1,11 @@
+
+package Demo.abstraction.interface26;
+
+public class main {
+    public static void main(String[] args) {
+        B ob2=new B();
+        ob2.display();
+        ob2.print();
+    }
+    
+}
