76bf184552cfbdfe41d9c0cc653b1f3e13ed51fe
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-20 01:49:07 +0600

    Update A.java

--- Java Abstraction/interface/p3/A.java
+++ Java Abstraction/interface/p3/A.java
@@ -1,1 +1,13 @@
-
+package Demo.abstraction.interface26.newpackage;
+interface A {
+     void display();
+   void print();
+}
+class B implements A{
+    public void display(){
+        System.out.println("cstu"); 
+    }
+   public void print(){
+       System.out.println("march");
+   }
+}
