05e25e22c8421676a61874a228fb43549f5b49f1
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-20 01:43:56 +0600

    Update and rename a.java to A.java

---
+++ Java Abstraction/interface/p1/A.java
@@ -0,0 +1,23 @@
+package Demo.abstraction.interface2;
+
+interface A {
+    void display();
+    
+}
+interface B extends A{
+    
+    void print();
+}
+class AB implements A,B{
+
+    @Override
+    public void display() {
+        System.out.println("cse department");
+    }
+
+    @Override
+    public void print() {
+        System.out.println("cstu iftar parti");
+    }
+    
+}
