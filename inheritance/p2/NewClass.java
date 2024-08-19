77b05e77911d0c68007f3ecab9ff361a39309442
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-20 01:30:03 +0600

    Update NewClass.java

--- inheritance/p2/NewClass.java
+++ inheritance/p2/NewClass.java
@@ -1,1 +1,12 @@
 
+package inheritance;
+
+public class NewClass {
+    public static void main(String[] args) {
+       
+         M ob = new M();
+        ob.display();
+        ob.print();
+    }
+}
+
