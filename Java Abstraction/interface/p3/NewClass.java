963c54c02f2846751479e72c39744aa7184aebb4
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-20 01:48:44 +0600

    Update NewClass.java

--- Java Abstraction/interface/p3/NewClass.java
+++ Java Abstraction/interface/p3/NewClass.java
@@ -1,1 +1,11 @@
 
+package Demo.abstraction.interface26.newpackage;
+
+public class NewClass {
+    public static void main(String[] args) {
+        B ob =new B();
+        ob.display();
+        ob.print();
+    }
+}
+
