97ed76703c97b3249694ff80f4ff4b285c27e18b
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-20 01:36:19 +0600

    Update Animal.java

--- Super keyword /p1/Animal.java
+++ Super keyword /p1/Animal.java
@@ -1,1 +1,25 @@
+package Supermal;
+
+class Animal {
+    int x = 100;
+
+    Animal() {
+        System.out.println("superclass");
+    }
+}
+
+class Dog extends Animal {
+    int x = 200;
+
+    Dog() {
+        super(); // Call to superclass constructor
+        System.out.println("childclass");
+    }
+    
+    // Define a display method to access x value
+    void display() {
+        System.out.println("x:" + super.x);
+    }
+}
+
 
