3006bacda9eab51bfb2ae7f7f02e922a7432b950
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-21 00:57:31 +0600

    Update Main.java

--- Java Polymorphism/Main.java
+++ Java Polymorphism/Main.java
@@ -1,1 +1,13 @@
+package polymorphism;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Bike obj= new Bike();
+		obj.run();
+		vehicle obj2=new Bike();
+		//Bike obj3=new vehicle();
+	}
+
+}
 
