d76f8ca8af22f9d24305ac7c608378327eedbafa
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-21 00:57:06 +0600

    Update vehicle.java

--- Java Polymorphism/vehicle.java
+++ Java Polymorphism/vehicle.java
@@ -1,1 +1,17 @@
+package polymorphism;
+/*Polymorphism in Java is a concept by which we can perform a single
+action in different ways. */
+public class vehicle {
+	public void run() {
+		System.out.println("running ");
+	}
+
+}
+class Bike extends vehicle{
+	public void run() {
+		System.out.println("running ");
+	}
+
+}
+
 
