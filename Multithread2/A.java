5ece6fb3604afcfaabeee7b566c4aee71c1cb1d9
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-21 00:39:32 +0600

    Update A.java

--- Multithread2/A.java
+++ Multithread2/A.java
@@ -1,1 +1,34 @@
+package learncoding;
+class A extends Thread{
+     public void run(){
+     String name=Thread.currentThread().getName();
+     for(int i=0;i<3;i++){
+         System.out.println(name);
+     }
+ }
+     public static void main(String[] args) throws UnsupportedOperationException, InterruptedException{
+         A t1=new A();
+         A t2=new A();
+         A t3=new A();
+         t1.setName("ratul");
+         t2.setName("rana");
+         t3.setName("rana");
+         
+         t1.start();
+          t2.start();
+        /* Thread.sleep(1000);
+         t2.suspend();
+         t2.resume();
+          Thread.sleep(1000);
+         t3.start();
+ Thread.sleep(1000); // Sleep for 1 second to let t2 print some messages
+   t2.suspend(); // Suspend t2
+   Thread.sleep(1000); // Sleep for 1 second to let t2 be suspended
+   t2.resume(); // Resume t2*/
+   t3.start();
+    Thread.sleep(1000);
+   t1.interrupt();
+     }
+    
+}
 
