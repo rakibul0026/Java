fc6713754b61bb3f954246b874f50c3c3645787c
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-21 01:00:36 +0600

    Update Vehicle.java

--- Super keyword /super problem/Vehicle.java
+++ Super keyword /super problem/Vehicle.java
@@ -1,1 +1,29 @@
+package javaprogram;
+
+public class Vehicle {
+	String color;
+	String name;
+	Vehicle(String color,String name){
+		this.color=color;
+		this.name=name;	
+	}
+	void display() {
+		System.out.println(color +" \n"+name);
+	}
+
+}
+class Bike extends Vehicle {
+	int bikeno;
+	Bike(String color,String name,int bikeno){
+		super(color ,name);
+		this.bikeno=bikeno;
+	}
+	
+
+ void display2(){
+	super.display();
+	System.out.println(bikeno);
+ }
+ }
+
 
