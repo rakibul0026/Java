479fe37c5b52fe8f235b914b5272cdf784bca89b
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-22 01:39:59 +0600

    Update main.java

--- Java Abstraction/p6/main.java
+++ Java Abstraction/p6/main.java
@@ -1,1 +1,16 @@
 
+package newpora;
+
+public class Mainclass {
+
+	public static void main(String[] args) {
+		Vehicle ob=new Car();
+		ob.startEngine();
+		ob.stopEngine();
+		
+		Vehicle ob2=new Motorcycle();
+		ob2.startEngine();
+		ob2.stopEngine();
+	}
+
+}
