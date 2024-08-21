11aabe0f53634f885df2eec701edfbdbece74f9a
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-22 01:00:22 +0600

    Update mobile.java

--- Java Abstraction/p3/mobile.java
+++ Java Abstraction/p3/mobile.java
@@ -1,1 +1,19 @@
+package abstract2;
+
+public abstract class mobile {
+	abstract void call();
+	void manage() 
+	{
+	System.out.println("messing ");
+
+    }
+}
+class user1 extends mobile
+{
+	void call() 
+	{
+	System.out.println("calling  ");
+
+    }	
+}
 
