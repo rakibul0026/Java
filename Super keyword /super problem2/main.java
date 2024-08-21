ec86e6155a76bbb2900e50433a002306aa3bb4fe
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-22 01:05:49 +0600

    Update main.java

--- Super keyword /super problem2/main.java
+++ Super keyword /super problem2/main.java
@@ -1,1 +1,8 @@
+package Supermal;
 
+public class Main {
+    public static void main(String[] args) {
+        Dog ob = new Dog();
+        ob.display(); // Call display method to see value of x
+    }
+}
