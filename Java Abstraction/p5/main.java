61da2856818c15e829820d729436e4aa0030b118
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-22 01:33:57 +0600

    Update main.java

--- Java Abstraction/p5/main.java
+++ Java Abstraction/p5/main.java
@@ -1,1 +1,13 @@
+package package2;
 
+public class main {
+
+	public static void main(String[] args) {
+		//Animal  ob=new Animal();//will get error
+		A ob= new 	A();
+		ob.sleep();
+		ob.display();
+
+	}
+
+}
