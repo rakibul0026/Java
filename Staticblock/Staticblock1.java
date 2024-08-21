72d378eb0ed7da4ec29d70743c1bc4c1be3ae6fa
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-22 01:52:57 +0600

    Update Staticblock1.java

--- Staticblock/Staticblock1.java
+++ Staticblock/Staticblock1.java
@@ -1,1 +1,22 @@
 
+
+
+package mycompany;
+
+public class Staticblock2 {
+String name;
+int id;
+static String u_name;
+Staticblock()
+{
+	
+	System.out.println("constructor");
+}
+}
+public Static void main(String[]args) {
+	static {
+		u_name="Cstu";
+		System.out.println("Static Block");
+	}
+}
+
