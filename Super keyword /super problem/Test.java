1d4b193c01f9dc86637983d99f8865c1a8faedc5
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-22 01:27:04 +0600

    Update Test.java

--- Super keyword /super problem/Test.java
+++ Super keyword /super problem/Test.java
@@ -2,11 +2,8 @@ package javaprogram;
 
 public class Main {
     public static void main(String[] args) {
-        
-        Bike myBike = new Bike("Red", "Yamaha", 12345);
-
-    
-        myBike.display2();
+         Bike myBike = new Bike("Red", "Yamaha", 12345);
+         myBike.display2();
     }
 }
 
