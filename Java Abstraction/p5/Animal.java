ff322ab57a4ed4de10d03e9167a43b83502f6d26
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-22 01:33:29 +0600

    Update Animal.java

--- Java Abstraction/p5/Animal.java
+++ Java Abstraction/p5/Animal.java
@@ -1,1 +1,16 @@
+package package2;
 
+public  abstract class Animal 
+ {
+public  abstract void display();
+public void sleep()
+  {
+	System.out.println("Cat");
+ }
+  }
+class A extends Animal {
+	public void display()
+	  {
+		System.out.println("tiger");
+	 }
+}
