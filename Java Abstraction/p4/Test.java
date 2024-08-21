d8b80673b4d9dbb8d018c10577a2475fbf0779f6
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-22 01:31:19 +0600

    Update Test.java

--- Java Abstraction/p4/Test.java
+++ Java Abstraction/p4/Test.java
@@ -1,1 +1,24 @@
+package w3problem3;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Manager ob1= new Manager();
+		ob1.name="rakibul";
+		ob1.baseSalary=2000;
+		ob1.department="cse";
+		
+		ob1.salary() ;
+		
+		Programmer ob2= new Programmer();
+		ob2.name="ratul";
+		ob2.baseSalary=2000;
+		ob2.hourlyRate=3;
+		//ob2.display1();
+		ob2.display() ;
+
+
+	}
+
+}
 
