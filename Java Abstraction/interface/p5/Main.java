1c1baab610ad58799977c96320c130e367d2889a
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-23 02:21:12 +0600

    Update Main.java

--- Java Abstraction/interface/p5/Main.java
+++ Java Abstraction/interface/p5/Main.java
@@ -1,1 +1,13 @@
+package demoproject;
+
+class Main {
+			  public static void main(String[] args) {
+			    DemoClass myObj = new DemoClass();
+			    myObj.myMethod();
+			    myObj.myOtherMethod();
+			  }
+			
+	}
+
+
 
