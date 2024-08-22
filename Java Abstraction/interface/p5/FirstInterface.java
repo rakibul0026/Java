0111d37091f1a4b2a1b000a92ffaec6cdfc870d3
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-23 02:20:46 +0600

    Update FirstInterface.java

--- Java Abstraction/interface/p5/FirstInterface.java
+++ Java Abstraction/interface/p5/FirstInterface.java
@@ -1,1 +1,19 @@
+package demoproject;
+
+interface FirstInterface {
+	  public void myMethod(); // interface method
+	}
+
+	interface SecondInterface {
+	  public void myOtherMethod(); // interface method
+	}
+
+	class DemoClass implements FirstInterface, SecondInterface {
+	  public void myMethod() {
+	    System.out.println("Some text.. for me ");
+	  }
+	  public void myOtherMethod() {
+	    System.out.println("Some other text... for me ");
+	  }
+}
 
