864019d841fefb215f706130d86ab1c0aab0135d
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-23 02:29:49 +0600

    Update stasticDemo.java

--- packagealpha/stasticDemo.java
+++ packagealpha/stasticDemo.java
@@ -1,1 +1,17 @@
 
+package packagealpha;
+
+public class stasticDemo {
+
+	public static void main(String[] args) {
+		Student ob1=new Student();
+		ob1.print();
+		Student ob2=new Student();
+		ob2.print();
+		Student ob3=new Student();
+		ob3.print();
+
+	}
+
+}
+
